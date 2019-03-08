a075a865008499e84fa855fb1dd8f89bf00fef2b
Yury <uburov@team.amocrm.com>
2019-03-08 12:12:20 +0300

    Добавление текстового поля

--- index.php
+++ index.php
@@ -8,34 +8,45 @@
 ini_set('max_execution_time', 2000);
 ini_set('display_errors','On');
 error_reporting('E_ALL');
-include 'layout.php';//Подключение макета формы.
-auth(); //Вызов авторизации.
-if(isset($_POST['submit'])){
-    $id_mult=add_custom_field('kk','5','1',[0=>'1',1=>'2',2=>'3',3=>'4',4=>'5',5=>'6',6=>'7',7=>'8',8 =>'9',9 => '10']);
+//Подключение макета формы.
+include 'layout.php';
+//Вызов авторизации.
+auth();
+//Обработка формы получения сущностей
+if(isset($_POST['submit_cont'])){
+    $id_mult=add_custom_field('5','1',[0=>'1',1=>'2',2=>'3',3=>'4',4=>'5',5=>'6',6=>'7',7=>'8',8 =>'9',9 => '10']);
     $count=$_POST['contact'];
+    //Разделеение по пакетам.
     while($count){
         if($count/500>1){
             $limit=500;
         }else $limit = $count;
         for($i=0;$i<$limit;$i++){
+            //Заполнение сущностей случайными значениями.
             $contacs[] = array('name' => create_essence());
             $leads[] = array('name' => create_essence());
             $companies[] = array('name' => create_essence());
             $customers[] = array('name' => create_essence());
+            //Получение массива случайно выбранных значений мультисписка.
             $random_arr[] = random_mult();
         }
+        //Получение массивов id
         $id_contacs = add($contacs, 'contacts');
         $id_leads = add($leads, 'leads');
         $id_companies = add($companies, 'companies');
         $id_customers = add($customers, 'customers');
         $id_multiselect = list_multyselect_id($id_mult);
-        select_multy($id_contacs,$random_arr,$id_multiselect,$id_mult);
-        $random_arr = [];
+        //Заполнение мультиселекта
+        select_multy('5',$id_contacs,$id_multiselect,$id_mult,$random_arr);
+
+        //Вызов функции связи.
         connection_essence($id_contacs,$id_companies,'company_id','contacts');
         connection_essence($id_contacs,$id_leads,'leads_id','contacts');
         connection_essence($id_contacs,$id_customers,'customers_id','contacts');
         connection_essence($id_companies,$id_leads,'leads_id','companies');
         connection_essence($id_companies,$id_customers,'customers_id','companies');
+        //Обнуление значений пакетов
+        $random_arr=[];
         $contacs=[];
         $leads=[];
         $companies=[];
@@ -43,12 +54,65 @@ if(isset($_POST['submit'])){
         $count-=$limit;
     }
 }
-if(isset($_POST['text']) and $_POST['id_essence']){
-    $id_text = $_POST['id_essence'];
-    add_custom_field($_POST['text'],'1',$id_text);
+//Обработка формы для изменения полей текст
+if(isset($_POST['text']) and $_POST['id_essence'] and $_POST['id_elem']){
+    $text = $_POST['text'];
+    $id_elem = $_POST['id_elem'];
+    $id_essence = $_POST['id_essence'];
+    $id_text = add_custom_field('1',$id_essence, $text);
+    var_dump($id_text);
+    add_text($id_essence,$id_text,$id_elem,$text);
 }
-//Проверка дублирования мультиселекта
-function check_mult($name,$type,$id_type){
+function add_text($id_essence,$id_text,$id_elem,$text){
+    $data = array (
+        'update' =>
+            array (
+                0 =>
+                    array (
+                        'id' => $id_elem,
+                        'updated_at' => '1552035720',
+                        'custom_fields' =>
+                            array (
+                                0 =>
+                                    array (
+                                        'id' => $id_text,
+                                        'values' =>
+                                            array (
+                                                0 =>
+                                                    array (
+                                                        'value' => $text,
+                                                    ),
+                                            ),
+                                    ),
+                            ),
+                    ),
+            ),
+    );
+    $arr_eccence = ['1'=>'contacts','2'=>'leads','3'=>'companies','12'=>'customers'];
+    $link = "https://uburov.amocrm.ru/api/v2/".$arr_eccence[$id_essence];
+
+    $headers[] = "Accept: application/json";
+
+    //Curl options
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_USERAGENT, "amoCRM-API-client-
+undefined/2.0");
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($curl, CURLOPT_URL, $link);
+    curl_setopt($curl, CURLOPT_HEADER,false);
+    curl_setopt($curl,CURLOPT_COOKIEFILE,dirname(__FILE__)."/cookie.txt");
+    curl_setopt($curl,CURLOPT_COOKIEJAR,dirname(__FILE__)."/cookie.txt");
+    $out = curl_exec($curl);
+    curl_close($curl);
+    $result = json_decode($out,TRUE);
+}
+function add_note(){
+
+}
+//Проверка дублирования поля
+function check_mult($type,$id_type){
     $link = 'https://uburov.amocrm.ru/api/v2/account?with=custom_fields';
 
     $headers[] = "Accept: application/json";
@@ -66,58 +130,52 @@ undefined/2.0");
     $out = curl_exec($curl);
     curl_close($curl);
     $result = json_decode($out,TRUE);
-    foreach ($result as $value){
-        var_dump($value);
-        echo '<br>';
-    }
     $essence = ['1'=>'contacts','2'=>'leads','3'=>'companies','12'=>'customers'];
     $custom_fields = $result['_embedded']['custom_fields'][$essence[$id_type]];
     foreach ($custom_fields as $value){
-        var_dump($value);
-        echo "<br>";
-        var_dump($name);
-        if($value['name'] == $name and $value['field_type'] == $type){
+
+        echo '<br>';
+        if($value['field_type'] == $type){
             return $value['id'];
         }
     }
     return null;
 }
-///Заполнение select
-function select_multy($id_contacs,$random_arr,$id_multiselect,$id_mult){
-    $arr = [];
-    $res = [];
-    foreach ($id_multiselect as $key => $value2){
-        $arr[] = $key;
-    }
-
-    foreach ($random_arr as $key=> $value){
-        foreach ($value as $key2 => $value2){
-            $res[$key][] = $arr[$value2];
+///Заполнение поля
+function select_multy($type_field,$id_contacs,$id_multiselect,$id_mult,$random_arr){
+        $arr = [];
+        $res = [];
+        foreach ($id_multiselect as $key => $value2){
+            $arr[] = $key;
         }
-       // var_dump($res[$key]);
-        //echo '<br>';
-    }
-    foreach ($id_contacs as $key => $value) {
-             $val[$key] = array (
-                             'id' => $id_contacs[$key],
-                             'updated_at' => time(),
-                             'custom_fields' =>
-                                 array (
-                                     0 =>array(
-                                            'id'=>$id_mult,
-                                            'values'=>$res[$key],
-                                     )
-                                 )
-             );
-             $result[] = $val[$key];
-             echo '<br>';
-    }
-    echo '<br>';
+        foreach ($random_arr as $key=> $value){
+            foreach ($value as $key2 => $value2){
+                $res[$key][] = $arr[$value2];
+            }
+            // var_dump($res[$key]);
+            //echo '<br>';
+        }
+        foreach ($id_contacs as $key => $value) {
+            $val[$key] = array(
+                'id' => $id_contacs[$key],
+                'updated_at' => time(),
+                'custom_fields' =>
+                    array(
+                        0 => array(
+                            'id' => $id_mult,
+                            'values' => $res[$key],
+                        ),
+                    ),
+            );
+            $result[] = $val[$key];
+        }
+
 
     $data = array (
         'update' =>
             $result,
     );
+
     $link = "https://uburov.amocrm.ru/api/v2/contacts";
 
     $headers[] = "Accept: application/json";
@@ -137,41 +195,39 @@ undefined/2.0");
     curl_close($curl);
     $result = json_decode($out,TRUE);
 }
-//Добавление мультиселекта
-function add_custom_field($name,$type,$id_element_type,$value){
-    if($type==5){
+//Добавление элемента поля
+function add_custom_field($type,$id_element_type,$value){
+    if($type=='5'){
         $fueld_enums = array (
-            'name' => $name,
+            'name' => 'Мультисписок',
             'type' => $type,
             'element_type' => $id_element_type,
             'origin' => '32',
             'enums' => $value,
         );
-    }else $fueld_enums  = array (
-            'name' => $name,
+    }else if($type=='1'){
+        $fueld_enums = array(
+            'name' => "Тестовое поле",
             'type' => $type,
             'element_type' => $id_element_type,
             'origin' => '32',
-    );
+        );
+    }
     $data = array (
         'add' =>
             array (
                 $fueld_enums,
             ),
     );
-    foreach ($data as $value){
-        //var_dump($value);
-        echo "<br>";
-    }
-    $fl = check_mult($data['add'][0]['name'],$data['add'][0]['type'],$id_element_type);
-    $link = "https://uburov.amocrm.ru/api/v2/fields";
+    //Проверка дублирования элемента
+    $fl = check_mult($type,$id_element_type);
 
-    $headers[] = "Accept: application/json";
     if(!$fl){
+        $link = "https://uburov.amocrm.ru/api/v2/fields";
+        $headers[] = "Accept: application/json";
         $result = curl($data,$headers,$link);
         return $result['_embedded']['items'][0]['id'];
     }else return $fl;
-
 }
 //Случаынй выбор полей мультиселекта
 function random_mult(){
